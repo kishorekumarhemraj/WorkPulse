9f5d8a26ae6f2f175b7314a075cca017a01144ec
Kishore Kumar Hemraj <kishorekumar.hemraj@outlook.com>
2026-08-25 01:00:10 +0530

    chore(release): bump minor version to 2.7.0

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 /// Application-wide constants for WorkPulse
 abstract class AppConstants {
   static const String appName = 'WorkPulse';
-  static const String appVersion = '2.6.8';
+  static const String appVersion = '2.7.0';
   static const String dbName = 'workpulse.db';
   static const int dbVersion = 4;
 
